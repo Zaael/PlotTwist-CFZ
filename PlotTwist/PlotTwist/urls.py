e53029f1b97f72121bbcd1fb1b69f3f456e9a5dd
Israel Souto dos Santos <zaael>
2019-05-24 01:25:52 -0300

    Criação do projeto e inicio de autentificação

--- PlotTwist/PlotTwist/urls.py
+++ PlotTwist/PlotTwist/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ..views.views import home_page, about_page, contact_page, login_page, register_page, home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', home),
+    path('produtos/', produtos),
+    path('', home_page),
+	path('about/', about_page),
+	path('contact/', contact_page),
+    path('login/', login_page),
+    path('register/', register_page),
 ]
